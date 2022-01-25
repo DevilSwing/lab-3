d27c634040f35bb5e5e0ed927f1c3a30f321a27b
429284-29 <429284-29>
2022-01-25 12:05:27 +0300

    res

---
+++ WpfApp1/DateTimeChart.xaml.cs
@@ -0,0 +1,60 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
+
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для DateTimeChart.xaml
+    /// </summary>
+    public partial class DateTimeChart : UserControl
+    {
+        public ChartValues<DataModel> ChartData { get; set; }
+
+        public void AddItem(double value, System.DateTime time)
+        {
+            ChartData.Add(new DataModel()
+            {
+                DateTime = time,
+                Value = value
+            });
+        }
+
+        public void Clear()
+        {
+            ChartData.Clear();
+        }
+
+        public DateTimeChart()
+        {
+            InitializeComponent();
+            ChartData = new ChartValues<DataModel>();
+            var dayConfig = Mappers.Xy<DataModel>()
+            .X(dayModel => dayModel.DateTime.Ticks)
+            .Y(dayModel => dayModel.Value);
+
+            Series = new SeriesCollection(dayConfig)
+            {
+                new LineSeries
+                {
+                    Title = ChartName,
+                    Values = ChartData
+                },
+            };
+            Formatter = value => new System.DateTime((long)value).ToString("yyyy-MM-dd HH:mm:ss");
+            DataContext = this;
+
+        }
+
+
+
+        public string ChartName { get; set; }
+
+        public Func<double, string> Formatter { get; set; }
+
+        public SeriesCollection Series { get; set; }
+    }
+}
